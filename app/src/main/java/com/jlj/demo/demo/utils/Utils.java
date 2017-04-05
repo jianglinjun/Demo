e1b3a990ef31a5c82e26dc5939f2fd19a3605185
jianglinjun <jianglinjun@zbj.com>
2017-04-06 00:13:53 +0800

    1.0.1_2

--- app/src/main/java/com/jlj/demo/demo/utils/Utils.java
+++ app/src/main/java/com/jlj/demo/demo/utils/Utils.java
@@ -2,26 +2,21 @@ package com.jlj.demo.demo.utils;
 
 import android.os.Build;
 
-<<<<<<< HEAD
-=======
 import java.util.UUID;
 
->>>>>>> developer_1.0.0
 /**
  * Created by jianglinjun on 17/4/5.
  */
 
 public class Utils {
-<<<<<<< HEAD
-    public static int getSDK(){
+    public static int getSDK() {
         return Build.VERSION.SDK_INT;
-=======
+    }
     public static String getUUID(){
         return UUID.randomUUID().toString();
     }
 
     public static String getUnique(){
         return Build.SERIAL;
->>>>>>> developer_1.0.0
     }
 }
