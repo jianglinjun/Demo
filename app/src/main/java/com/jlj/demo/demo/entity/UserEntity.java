746c5e6c9e0c14f5d45ac57cbd48bd84381ac023
jianglinjun <jianglinjun@zbj.com>
2017-04-05 23:50:31 +0800

    1.0.0

---
+++ app/src/main/java/com/jlj/demo/demo/entity/UserEntity.java
@@ -0,0 +1,35 @@
+package com.jlj.demo.demo.entity;
+
+/**
+ * Created by jianglinjun on 17/4/5.
+ */
+
+public class UserEntity {
+    private String id = null;
+    private String name = null;
+    private int age = 0;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ app/src/main/java/com/jlj/demo/demo/utils/Utils.java
@@ -0,0 +1,13 @@
+package com.jlj.demo.demo.utils;
+
+import java.util.UUID;
+
+/**
+ * Created by jianglinjun on 17/4/5.
+ */
+
+public class Utils {
+    public static String getUUID(){
+        return UUID.randomUUID().toString();
+    }
+}
