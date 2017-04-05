e246bce2add3a121fd173e7a881f33801c22f34a
jianglinjun <jianglinjun@zbj.com>
2017-04-05 23:57:21 +0800

    1.0.1_1

---
+++ app/src/main/java/com/jlj/demo/demo/utils/Utils.java
@@ -0,0 +1,13 @@
+package com.jlj.demo.demo.utils;
+
+import android.os.Build;
+
+/**
+ * Created by jianglinjun on 17/4/5.
+ */
+
+public class Utils {
+    public static int getSDK(){
+        return Build.VERSION.SDK_INT;
+    }
+}
